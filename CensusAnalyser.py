9b8cce875433fec374594ed847c8561f8f75c300
AkankshaKaple <akankshakaple@gmail.com>
2020-12-04 11:28:09 +0530

    Refactored: Created methods to load Indian Census data and State Census data, and return number of records

--- CensusAnalyser.py
+++ CensusAnalyser.py
@@ -1,14 +1,38 @@
 import pandas as pd
 
+from CensusAnalyserError import CensusAnalyserError
+from IndianCensusCSV import IndiaCensusCSV, StateCensusCSV
+
+
 class CSVLoader:
 
     def __init__(self, path):
         self.path = path
 
-    def record_counter(self):
+    def load_census_data(self):
         """
-        Count record in file
+        Load census data and return number of 
         :return: number of records in file
         """
-        data = pd.read_csv(self.path)
-        return len(data)
+        try:
+            census_col_list = repr(IndiaCensusCSV()).split(",")
+            census_data = pd.read_csv(self.path, usecols=census_col_list)
+            return len(census_data)
+        except FileNotFoundError:
+            raise CensusAnalyserError("Check file path")
+        except ValueError:
+            raise CensusAnalyserError("Wrong delimiter Or Headers do not match")
+
+    def load_state_census_data(self):
+        """
+        Load state census data and return number of
+        :return: number of records in file
+        """
+        try:
+            state_col_list = repr(StateCensusCSV()).split(",")
+            state_data = pd.read_csv(self.path, usecols=state_col_list)
+            return len(state_data)
+        except FileNotFoundError:
+            raise CensusAnalyserError("Check file path")
+        except ValueError:
+            raise CensusAnalyserError("Wrong delimiter Or Headers do not match")
