628cbf04dca053422ad76590d45a51d953b86093
AkankshaKaple <akankshakaple@gmail.com>
2020-12-04 12:21:33 +0530

    Refactored: Added class method to sort data by statename

--- CensusAnalyser.py
+++ CensusAnalyser.py
@@ -1,16 +1,27 @@
 from CsvLoader import CSVLoader
+import pandas as pd
+from IndianCensusCSV import IndiaCensusCSV
+
 
 class CensusAnalyser:
+    __data_list = pd.DataFrame()
 
     def __init__(self, path):
         self.path = path
 
     def census_record_counter(self):
-        indian_census_records = CSVLoader.load_indian_census_data(self.path)
-        return len(indian_census_records)
+        CensusAnalyser.__data_list = CSVLoader.load_indian_census_data(self.path)
+        return len(CensusAnalyser.__data_list)
 
     def state_code_record_counter(self):
-        state_census_records = CSVLoader.load_state_code_data(self.path)
-        return len(state_census_records)
+        CensusAnalyser.__data_list = CSVLoader.load_state_code_data(self.path)
+        return len(CensusAnalyser.__data_list)
+
+    @classmethod
+    def sort_by_state(cls):
+        CensusAnalyser.__data_list.sort_values(by=[IndiaCensusCSV().state], inplace=True)
+        return CensusAnalyser.__data_list.to_json(orient='records')
+
+
 
 
