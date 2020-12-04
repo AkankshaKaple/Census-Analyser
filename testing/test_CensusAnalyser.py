6667f59db0841fa3ef2799915f5ecc4d93efd406
AkankshaKaple <akankshakaple@gmail.com>
2020-12-04 10:52:34 +0530

    Added: Test cases for UC1

---
+++ testing/test_CensusAnalyser.py
@@ -0,0 +1,39 @@
+from CensusAnalyserError import CensusAnalyserError
+from CensusAnalyser import CSVLoader
+import pytest
+
+CENSUS_CSV_FILE_PATH = "/home/akanksha/PycharmProjects/CensusAnalyzer/Data/IndiaStateCensusData.csv"
+CENSUS_CSV_FILE_WRONG_PATH = "IndiaStateCensusData.csv"
+CENSUS_CSV_FILE_WRONG_TYPE = "/home/akanksha/PycharmProjects/CensusAnalyzer/Data/USCensusData.xls"
+
+# NOTE - Change delimiter of the file and save it
+CENSUS_CSV_FILE_WRONG_DELIMITER = "/home/akanksha/PycharmProjects/CensusAnalyzer/Data/USCensusDataDelimiter.csv"
+
+
+# check if length of records is same or not
+def test_record_counter():
+    csv_loader = CSVLoader(CENSUS_CSV_FILE_PATH)
+    assert csv_loader.record_counter() == 29
+
+
+# Check if exception gets raised or not
+def test_record_counter_for_wrong_file_path():
+    csv_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_PATH)
+    with pytest.raises(CensusAnalyserError):
+        csv_loader.record_counter()
+
+
+# Check if exception gets raised or not
+def test_record_counter_for_wrong_file_type():
+    csv_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_TYPE)
+    with pytest.raises(CensusAnalyserError):
+        csv_loader.record_counter()
+
+
+# Check if exception gets raised or not
+# NOTE - Change delimiter of the file and save it
+def test_record_counter_for_wrong_delimiter():
+    csv_loader = CSVLoader(CENSUS_CSV_FILE_WRONG_DELIMITER)
+    with pytest.raises(CensusAnalyserError):
+        csv_loader.record_counter()
+
