23ca68aec8e522230997e83183499fd413d66e90
AkankshaKaple <akankshakaple@gmail.com>
2020-12-04 11:01:31 +0530

    Refactor: Added Exception Handling

--- CensusAnalyser.py
+++ CensusAnalyser.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from CensusAnalyserError import CensusAnalyserError
+from IndianCensusCSV import IndiaCensusCSV
+
 class CSVLoader:
 
     def __init__(self, path):
@@ -10,5 +13,11 @@ class CSVLoader:
         Count record in file
         :return: number of records in file
         """
-        data = pd.read_csv(self.path)
-        return len(data)
+        try:
+            state_col_list = repr(IndiaCensusCSV()).split(",")
+            state_list = pd.read_csv(self.path, usecols=state_col_list)
+            return len(state_list)
+        except FileNotFoundError:
+            raise CensusAnalyserError("Check file path")
+        except ValueError:
+            raise CensusAnalyserError("Wrong delimiter Or Headers do not match")
